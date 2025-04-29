2883808dbbf9a7c53c54e06915b7adc8f13c6c3f
Gian Pena <flaconumero2@gmail.com>
2025-04-29 16:14:35 -0400

    Update and rename download script

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 source .venv/bin/activate
-#python StatsFetcher.py 15 > time15.txt
-#sleep 8
+python StatsFetcher.py 15 > time15.txt
+sleep 8
 python StatsFetcher.py 60 > time60.txt
