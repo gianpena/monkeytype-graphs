99192319113c973ff5e2dd423cac5b6b9ee0c030
Gian Peña <flaconumero2@gmail.com>
2025-08-14 01:27:03 -0400

    add new stats fetcher

---
+++ fetch-stats.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+async function writeStatsToFile(stats) {
+    
+    const filePath = `./${args[0]}${args[1]}.json`;
+    try {
+        const fileContent = fs.existsSync(filePath) ? fs.readFileSync(filePath, 'utf8') : '[]';
+        const existingStats = [JSON.parse(fileContent), ...stats ];
+        fs.writeFileSync(filePath, JSON.stringify(existingStats, null, 2));
+    } catch (err) {
+        console.error(`Error reading or writing ${filePath}:`, err);
+    }
+
+}
+
+let page = 1;
+
+async function fetchStats() {
+    
+    let isPageComplete = true;
+    let lastPage = page + 500;
+    for(; isPageComplete && page < lastPage; ++page) {
+        const requestStr = `https://api.monkeytype.com/leaderboards?language=english&mode=${args[0]}&mode2=${args[1]}&page=${page}&pageSize=200`;
+        const response = await fetch(requestStr);
+        if(!response.ok) {
+            console.log(`Something went wrong on the following request:\n${requestStr}`);
+            return;
+        }
+        const { data } = await response.json();
+        if(!data) {
+            console.log(`Request successful but no data found:\n${requestStr}`);
+            return;
+        }
+        const { entries } = data;
+        await writeStatsToFile(entries);
+        
+        isPageComplete = data && entries.length === 200;
+
+    }
+
+    return isPageComplete;
+
+}
+
+const callback = async () => {
+    const isComplete = await fetchStats();
+    if(isComplete) process.exit(0);
+};
+callback();
+setInterval(callback, 1 * 60 * 60 * 1000 + 10000);
